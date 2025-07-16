742d836783061752e5903027909d9b97c877202f
Bryan <60058743+limjunan@users.noreply.github.com>
2025-07-17 00:46:48 +0800

    refactor: AddFormElements and FormBuilder into one component

---
+++ src/components/builder/EditForm.tsx
@@ -0,0 +1,32 @@
+import { type FormElement } from '../../types'
+import AddFormElements from './AddFormElements'
+import FormBuilder from './FormBuilder'
+
+interface FormEditorProps {
+  formElements: FormElement[]
+  addElement: (type: 'text' | 'paragraph' | 'checkbox' | 'select') => void
+  removeElement: (id: string) => void
+  setFormElements: (elements: FormElement[]) => void
+}
+
+export default function EditForm({
+  formElements,
+  addElement,
+  removeElement,
+  setFormElements,
+}: FormEditorProps) {
+  return (
+    <div className="flex flex-col lg:flex-row gap-8">
+      <div className="lg:w-1/3 fixed bottom-0 left-0 right-0 bg-surface-1 px-8 py-2 lg:static lg:p-0 lg:bg-transparent z-50">
+        <AddFormElements onAddElement={addElement} />
+      </div>
+      <div className="lg:w-2/3 pb-32 lg:pb-0">
+        <FormBuilder
+          formElements={formElements}
+          onRemoveElement={removeElement}
+          onUpdateElements={setFormElements}
+        />
+      </div>
+    </div>
+  )
+}
