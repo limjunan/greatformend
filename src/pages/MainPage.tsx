907544067111ff3d67081a8ea0884631f393c0f5
Bryan <60058743+limjunan@users.noreply.github.com>
2025-07-16 22:53:24 +0800

    feat: add form builder component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,20 @@
 import Header from '../components/Header'
 import AddFormElements from '../components/AddFormElements'
+import FormBuilder from '../components/FormBuilder'
 
 export default function MainPage() {
   return (
     <div className="min-h-screen bg-surface-2">
       <Header />
-      <div className="max-w-2xl mx-auto p-8">
-        <AddFormElements />
-        <p className="text-text-secondary mb-6">
-          Your React + TypeScript + Tailwind CSS app is ready!
-        </p>
+      <div className="max-w-6xl mx-auto p-8">
+        <div className="flex flex-col lg:flex-row gap-8">
+          <div className="lg:w-1/3">
+            <AddFormElements />
+          </div>
+          <div className="lg:w-2/3">
+            <FormBuilder />
+          </div>
+        </div>
       </div>
     </div>
   )
